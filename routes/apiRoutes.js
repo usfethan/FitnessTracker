de991ce6cdd5676737852637efb0a94882cf2660
Fethan Us <usfethan@gmail.com>
2021-12-17 09:42:13 -0500

    add exercise added

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -19,3 +19,23 @@ router.get("/api/workouts", (req, res) => {
     });
 });
 
+//add exercise
+
+router.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findOneAndUpdate(
+        {
+            _id: req.params.id
+        },
+        {
+            $inc: {totalDuration: req.body.duration},
+            $push: {exercises: req.body}
+        },
+        {new: true}
+    ) .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
+
