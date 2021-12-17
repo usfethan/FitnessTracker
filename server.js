58e4e1a4885d8cd8079d9d11ccb315d7218bcdd5
Fethan Us <usfethan@gmail.com>
2021-12-16 21:07:50 -0500

    created server and link all require. created routes files

---
+++ server.js
@@ -0,0 +1,29 @@
+const exp = require("constants");
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+const PORT = process.env.PORT || 3000;
+const db = require("./models");
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+//--------
+
+//mongoose connection ???
+
+//--------
+
+//Routes//
+//get workouts
+//add exercise
+//create workout
+//get stats
+
+
+app.listen(3000, () => {
+    console.log(`Server running on port ${PORT}`);
+});
